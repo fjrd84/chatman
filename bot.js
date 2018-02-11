4fea208476f13e7f26d523c5fcce63a1bc2ef3a7
F. Javier R. Donado <fjrd84@gmail.com>
2018-02-11 10:38:02 +0100

    Minimal offline working example.

--- bot.js
+++ bot.js
@@ -1,40 +1,72 @@
-/*~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-           ______     ______     ______   __  __     __     ______
-          /\  == \   /\  __ \   /\__  _\ /\ \/ /    /\ \   /\__  _\
-          \ \  __<   \ \ \/\ \  \/_/\ \/ \ \  _"-.  \ \ \  \/_/\ \/
-           \ \_____\  \ \_____\    \ \_\  \ \_\ \_\  \ \_\    \ \_\
-            \/_____/   \/_____/     \/_/   \/_/\/_/   \/_/     \/_/
-
-
-# EXTEND THE BOT:
-
-  Botkit has many features for building cool and useful bots!
-
-  Read all about it here:
-
-    -> http://howdy.ai/botkit
-
-~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~*/
 var env = require('node-env-file');
 env(__dirname + '/.env');
-
 
 var Botkit = require('botkit');
 var debug = require('debug')('botkit:main');
 
+const FORTUNE_COOKIES = [
+    'Today it\'s up to you to create the peacefulness you long for.',
+    'A friend asks only for your time not your money.',
+    'If you refuse to accept anything but the best, you very often get it.',
+    'A smile is your passport into the hearts of others.',
+    'A good way to keep healthy is to eat more Chinese food.',
+    'Your high-minded principles spell success.',
+    'Hard work pays off in the future, laziness pays off now.',
+    'Change can hurt, but it leads a path to something better.',
+    'Enjoy the good luck a companion brings you.',
+    'People are naturally attracted to you.',
+    'Hidden in a valley beside an open stream- This will be the type of place where you will find your dream.',
+    'A chance meeting opens new doors to success and friendship.',
+    'You learn from your mistakes... You will learn a lot today.',
+    'If you have something good in your life, don\'t let it go!',
+    'What ever you\'re goal is in life, embrace it visualize it, and for it will be yours.',
+    'Your shoes will make you happy today.',
+    'You cannot love life until you live the life you love.',
+    'Be on the lookout for coming events; They cast their shadows beforehand.',
+    'Land is always on the mind of a flying bird.',
+    'The man or woman you desire feels the same about you.',
+    'Meeting adversity well is the source of your strength.',
+    'A dream you have will come true.',
+    'Our deeds determine us, as much as we determine our deeds.',
+    'Never give up. You\'re not a failure if you don\'t give up.',
+    'You will become great if you believe in yourself.',
+    'There is no greater pleasure than seeing your loved ones prosper.',
+    'You will marry your lover.',
+    'A very attractive person has a message for you.',
+    'You already know the answer to the questions lingering inside your head.',
+    'It is now, and in this world, that we must live.',
+    'You must try, or hate yourself for not trying.',
+    'You can make your own happiness.',
+    'The greatest risk is not taking one.',
+    'The love of your life is stepping into your planet this summer.',
+    'Love can last a lifetime, if you want it to.',
+    'Adversity is the parent of virtue.',
+    'Serious trouble will bypass you.',
+    'A short stranger will soon enter your life with blessings to share.',
+    'Now is the time to try something new.',
+    'Wealth awaits you very soon.',
+    'If you feel you are right, stand firmly by your convictions.',
+    'If winter comes, can spring be far behind?',
+    'Keep your eye out for someone special.',
+    'You are very talented in many ways.',
+    'A stranger, is a friend you have not spoken to yet.',
+    'A new voyage will fill your life with untold memories.',
+    'You will travel to many exotic places in your lifetime.',
+    'Your ability for accomplishment will follow with success.',
+    'Nothing astonishes men so much as common sense and plain dealing.',
+    'Its amazing how much good you can do if you dont care who gets the credit.'
+];
+
 var bot_options = {
-    studio_token: process.env.studio_token,
-    studio_command_uri: process.env.studio_command_uri,
-    studio_stats_uri: process.env.studio_command_uri,
     replyWithTyping: true,
 };
 
 // Use a mongo database if specified, otherwise store in a JSON file local to the app.
 // Mongo is automatically configured when deploying to Heroku
 if (process.env.MONGO_URI) {
-  // create a custom db access method
-  var db = require(__dirname + '/components/database.js')({});
-  bot_options.storage = db;
+    // create a custom db access method
+    var db = require(__dirname + '/components/database.js')({});
+    bot_options.storage = db;
 } else {
     bot_options.json_file_store = __dirname + '/.data/db/'; // store user data in a simple JSON format
 }
@@ -45,17 +77,8 @@ var controller = Botkit.socketbot(bot_options);
 // Set up an Express-powered webserver to expose oauth and webhook endpoints
 var webserver = require(__dirname + '/components/express_webserver.js')(controller);
 
-
-
-// Load in some helpers that make running Botkit on Glitch.com better
-require(__dirname + '/components/plugin_glitch.js')(controller);
-
 // Load in a plugin that defines the bot's identity
 require(__dirname + '/components/plugin_identity.js')(controller);
-
-// enable advanced botkit studio metrics
-// and capture the metrics API to use with the identity plugin!
-controller.metrics = require('botkit-studio-metrics')(controller);
 
 // Open the web socket server
 controller.openSocketServer(controller.httpserver);
@@ -63,58 +86,23 @@ controller.openSocketServer(controller.httpserver);
 // Start the bot brain in motion!!
 controller.startTicking();
 
-
 var normalizedPath = require("path").join(__dirname, "skills");
-require("fs").readdirSync(normalizedPath).forEach(function(file) {
-  require("./skills/" + file)(controller);
+require("fs").readdirSync(normalizedPath).forEach(function (file) {
+    require("./skills/" + file)(controller);
 });
 
-console.log('I AM ONLINE! COME TALK TO ME: http://localhost:' + (process.env.PORT || 3000))
+console.log('I AM ONLINE! COME TALK TO ME: http://localhost:' + (process.env.PORT || 3000));
 
-// This captures and evaluates any message sent to the bot as a DM
-// or sent to the bot in the form "@bot message" and passes it to
-// Botkit Studio to evaluate for trigger words and patterns.
-// If a trigger is matched, the conversation will automatically fire!
-// You can tie into the execution of the script using the functions
-// controller.studio.before, controller.studio.after and controller.studio.validate
-if (process.env.studio_token) {
-    controller.on('message_received', function(bot, message) {
-        controller.studio.runTrigger(bot, message.text, message.user, message.channel, message).then(function(convo) {
-            if (!convo) {
-              // web bot requires a response of some kind!
-              bot.reply(message,'OK');
-
-                // no trigger was matched
-                // If you want your bot to respond to every message,
-                // define a 'fallback' script in Botkit Studio
-                // and uncomment the line below.
-                // controller.studio.run(bot, 'fallback', message.user, message.channel, message);
-            } else {
-                // set variables here that are needed for EVERY script
-                // use controller.studio.before('script') to set variables specific to a script
-                convo.setVar('current_time', new Date());
-                convo.setVar('bot', controller.studio_identity);
-            }
-        }).catch(function(err) {
-            bot.reply(message, 'I experienced an error with a request to Botkit Studio: ' + err);
-            debug('Botkit Studio: ', err);
-        });
-    });
-} else {
-
-    console.log('~~~~~~~~~~');
-    console.log('NOTE: Botkit Studio functionality has not been enabled');
-    console.log('To enable, pass in a studio_token parameter with a token from https://studio.botkit.ai/');
-}
-
-
-
-
-function usage_tip() {
-    console.log('~~~~~~~~~~');
-    console.log('Botkit Starter Kit');
-    console.log('Execute your bot application like this:');
-    console.log('PORT=3000 studio_token=<MY BOTKIT STUDIO TOKEN> node bot.js');
-    console.log('Get a Botkit Studio token here: https://studio.botkit.ai/')
-    console.log('~~~~~~~~~~');
-}
+controller.on('message_received', function (bot, message) {
+    let answer = FORTUNE_COOKIES[
+        Math.round(
+            Math.random() * (FORTUNE_COOKIES.length - 1)
+        )
+    ];
+    bot.reply(message,
+        {
+            text: answer,
+            typingDelay: 500,
+        }
+    );
+});
